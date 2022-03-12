3abe45fd2cb6c63bcc490648179a2388893d6b1a
Alexey Kvasov <alexeykvasovn7@gmail.com>
2022-03-12 21:55:27 +0200

    added socket connection using message form

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const massage = e.target.elements.message.value
+    socket.emit('sendMassage', massage)
+})
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const path = require('path')
 const http = require('http')
-const { Server } = require("socket.io");
+const { Server } = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server);
+const io = new Server(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-});
+    console.log('a user connected')
+    socket.emit('message', 'Welcome!')
+    socket.on('sendMassage', (message) => {
+        io.emit('message', message)
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server is started on port: ${port}`)
